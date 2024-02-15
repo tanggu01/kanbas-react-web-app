b4bad7147f2464fd78a9a281ae59f8920069c9bd
taehyun <tanggu01@connect.hku.hk>
2024-02-14 22:25:22 -0500

    :see_no_evil: update .gitignore

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Labs from "./Labs";
+import Kanbas from "./Kanbas";
+import HelloWorld from "./Labs/a3/HelloWorld";
+
+function App() {
+  return (
+      <div>
+        <Labs/>
+        <Kanbas/>
+        <HelloWorld/>
+      </div>
+  );
+}
+
+export default App;
